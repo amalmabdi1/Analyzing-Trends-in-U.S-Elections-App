a0bbaf51f9608460061adec049947f212a6b5d17
“Amal <aa4569a@american.edu>
2025-12-06 12:10:42 -0500

    Data clean up

--- AOSapp.R
+++ AOSapp.R
@@ -1,6 +1,33 @@
-library(readr)
-president <- read_csv("data_raw/1976-2020-president.csv")
+library(reader)
+library(dplyr)
 
-senate <- read_csv("data_raw/1976-2020-senate.csv")
+state_election_df <- read.csv("/Users/amalabdi/Downloads/correlates2-6.csv", header = TRUE)
 
-house <- read_csv("data_raw/1976-2024-house.csv")
+names(state_election_df)
+
+
+    
+gov_df <- state_election_df %>%
+      mutate(
+        govparty = coalesce(
+          govparty_c,
+          govparty_b_2,
+          govparty_b,
+          govparty_a
+        )
+      ) %>%
+      filter(year >= 1975) %>%
+      select(state, year, govparty)
+
+
+gov_df <-  gov_df |>
+  mutate(
+    govparty_label = case_when(
+      govparty == 0 ~ "Republican",
+      govparty == 1 ~ "Democratic",
+      govparty == .5 ~ "Independent/Third Party",
+      TRUE ~ NA_character_
+    )
+  )
+
+View(gov_df)
